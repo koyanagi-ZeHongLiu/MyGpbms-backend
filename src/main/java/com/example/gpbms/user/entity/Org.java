37aeb21bb1280fe958296155b7f498ae3f9bdde6
Koyanagi <13015853039@163.com>
2019-07-23 22:17:55 +0800

    将org的管理员设置成默认NULL

--- src/main/java/com/example/gpbms/user/entity/Org.java
+++ src/main/java/com/example/gpbms/user/entity/Org.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -12,6 +14,8 @@ import java.util.List;
 
 @Data
 @Entity
+@DynamicInsert
+@DynamicUpdate  //这两个注解是为属性设置默认值时用到的
 @NoArgsConstructor
 @Accessors(chain = true)
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
@@ -32,13 +36,13 @@ public class Org {
     /*@JsonIgnore
     @OneToOne(fetch = FetchType.LAZY)
     private User purchaseAdmin;*/
-    @Column(name = "purchase_admin")
+    @Column(name = "purchase_admin",columnDefinition = "varchar(32) default null")
     private String purchaseAdmin;
 
     /*@JsonIgnore
     @OneToOne(fetch = FetchType.LAZY)
     private User orgAdmin;*/
-    @Column(name = "org_admin")
+    @Column(name = "org_admin",columnDefinition = "varchar(32) default null")
     private String orgAdmin;
 
 
